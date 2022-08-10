4f21f150df7349e8e11a699724d50c556d31c95f
Vladislav <vladislavgalkin5@gmail.com>
2022-08-11 02:44:38 +0300

    send data from B to A

--- UIColor/UIColor/Controllers/FirstVC.swift
+++ UIColor/UIColor/Controllers/FirstVC.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class FirstVC: UIViewController {
+// MARK: - Protocols
+
+protocol DataUpdateProtocol {
+    func onDataUpdate (data: UIColor)
+}
+
+final class FirstVC: UIViewController, DataUpdateProtocol {
+    
+    // MARK: - Properties
+    @IBOutlet private var viewOut: UIView!
     
     // MARK: - Action
     
     @IBAction func changeBGBtnAction(_ sender: UIButton) {
-        performSegue(withIdentifier: "goToSecondVC", sender: nil)
+        // получаем вью контроллер
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let editScreen = storyboard.instantiateViewController(withIdentifier:
+        "SecondVC") as! SecondVC
+         // передаем данные
+        editScreen.setColor = viewOut.backgroundColor
+        // устанавливаем текущий класс в качестве делегата
+        editScreen.dalegate = self
+        // открываем следующий экран
+        self.navigationController?.pushViewController(editScreen, animated: true)
+    }
+    
+    // MARK: - Functions
+    
+    func onDataUpdate(data: UIColor) {
+        viewOut.backgroundColor = data
     }
     
     // MARK: - Navigation
---
+++ UIColor/UIColor/Models/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  UIColor
+//
+//  Created by Владислав on 11.08.22.
+//
+
+import UIKit
+
+struct Color {
+    var r: CGFloat = 0.0
+    var g: CGFloat = 0.0
+    var b: CGFloat = 0.0
+    var o: CGFloat = 0.0
+}
